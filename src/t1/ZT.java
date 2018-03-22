97a28f00526b538dc96900f2493eb6cbc1eee5ae
Vinay <vinay.rana2603@gmail.com>
2018-03-22 09:43:43 +0530

    3rd commit

--- src/t1/ZT.java
+++ src/t1/ZT.java
@@ -6,6 +6,7 @@ public class ZT {
 		// TODO Auto-generated method stub
 		System.out.println("ZT! Project");
 		System.out.println("Added to check 1");
+		System.out.println("Added to check 2");
 
 	}
 
